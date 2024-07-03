a0a85a652183b0fe639697f65042055de0aa250a
Thiago Feldmann <thiagofeldmann06@gmail.com>
2024-07-03 14:02:23 -0300

    resolução do exercicio

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicação(a, b) {
+    return a * b;
+}
+console.log(multiplicação(10, 4)); // declarando os valores para a multiplicação
+function saudação(nome) {
+    return "Olá " + nome + "!";
+}
+console.log(saudação('Fulano')); // declarando o valor para a saudação
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+
+function multiplicação(a: number, b: number) { // função multiplicativa
+    return a * b;
+}
+
+console.log(multiplicação(10, 4)); // declarando os valores para a multiplicação
+
+function saudação(nome: string) { // função de saudação
+    return "Olá " + nome + "!";
+}
+
+console.log(saudação('Fulano')); // declarando o valor para a saudação
